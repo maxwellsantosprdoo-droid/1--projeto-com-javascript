a1d0dafcd31533ade90e5bad598213653f6a8ac3
maxwellsantosprdoo-droid <maxwellsantosprdoo@gmail.com>
2026-02-25 21:29:43 -0300

    mudanças ecenciais

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
+
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value;
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
@@ -8,6 +9,8 @@ function convertValues() {
 
     const dolarToday = 5.2;
     const euroToday = 6.2;
+    const libraToday = 7.2;
+    const bitcoinToday = 326497.08;
 
 
 
@@ -26,7 +29,20 @@ function convertValues() {
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday);
     }
-
+if (currencySelect.value == "libra") {
+        //se o valor estiver selecionado o valotr de libra,entra nesse if e converte o valor para libra
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday);
+    }
+    if (currencySelect.value == "bitcoin") {
+        //se o valor estiver selecionado o valotr de bitcoin,entra nesse if e converte o valor para bitcoin
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputCurrencyValue / bitcoinToday);
+    }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -47,6 +63,15 @@ function changeCurrency() {
         currencyName.innerHTML = "Euro";
         currencyImage.src = "./img/euro-2.png";
 
+
+    }
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra Esterlina";
+        currencyImage.src = "./img/libra-1.png";
+    }
+    if (currencySelect.value == "bitcoin") {
+        currencyName.innerHTML = "Bitcoin";
+        currencyImage.src = "./img/bitcoin-1.png";
     }
     convertValues();
 }
